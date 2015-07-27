47e2547c7a42876df4dee93aa1e05afa5ae30aa0
nwolek <nw@nathanwolek.com>
2015-07-27 12:48:15 -0400

    JamomaDcblock.h doxygen header - see issue #14

--- includes/JamomaDcblock.h
+++ includes/JamomaDcblock.h
@@ -1,10 +1,17 @@
-//
-//  JamomaObject.h
-//  JamomaExperiments
-//
-//  Created by Timothy Place on 3/27/15.
-//  Copyright (c) 2015 jamoma. All rights reserved.
-//
+/** @file
+ *
+ * @ingroup jamoma2
+ *
+ * @brief Block DC offset within samples.
+ *
+ * @details This #AudioObject removes DC offset (sometimes called <a href="https://en.wikipedia.org/wiki/DC_bias">DC bias</a> from a #Sample or #SampleBundle. The process is acheived by applying a first-order highpass filter to the input.
+ *
+ * @author Timothy Place, Nathan Wolek
+ *
+ * @copyright Copyright © 2015 by Jamoma authors and contributors @n
+ * This code is licensed under the terms of the "BSD 3-Clause License" @n
+ * https://github.com/jamoma/jamoma2/blob/master/LICENSE.md @n
+ */
 
 #pragma once
 
