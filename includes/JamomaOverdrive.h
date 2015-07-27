dcedc12f5e7957e1e04d832574d540b5569d7d80
nwolek <nw@nathanwolek.com>
2015-07-27 13:36:48 -0400

    JamomaOverdrive.h doxygen header - see issue #14

--- includes/JamomaOverdrive.h
+++ includes/JamomaOverdrive.h
@@ -1,10 +1,19 @@
-//
-//  JamomaObject.h
-//  JamomaExperiments
-//
-//  Created by Timothy Place on 3/27/15.
-//  Copyright (c) 2015 jamoma. All rights reserved.
-//
+/** @file
+ *
+ * @ingroup jamoma2
+ *
+ * @brief Apply a soft saturation distortion effect to samples.
+ *
+ * @details This #AudioObject "warms" the signal by applying a small amount of <a href="https://en.wikipedia.org/wiki/Distortion_(music)">distortion</a> to a #Sample or #SampleBundle. It contains two options for the underlying processing algorithm and three options for clipping the initial input.
+ *
+ * @author Timothy Place, Nathan Wolek
+ *
+ * @warning This class is a stub.
+ *
+ * @copyright Copyright © 2015 by Jamoma authors and contributors @n
+ * This code is licensed under the terms of the "BSD 3-Clause License" @n
+ * https://github.com/jamoma/jamoma2/blob/master/LICENSE.md @n
+ */
 
 #pragma once
 
