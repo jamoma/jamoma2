79ecaf0a6b320979bef06946e275fde90421d0fb
nwolek <nw@nathanwolek.com>
2015-07-27 11:59:19 -0400

    JamomaValue.h doxygen header - see issue #14

--- includes/JamomaValue.h
+++ includes/JamomaValue.h
@@ -1,3 +1,17 @@
+/** @file
+ *
+ * @ingroup jamoma2
+ *
+ * @brief Represents an arbitrary type of value.
+ *
+ * @details Defines the primary interface used to pass values to and from methods in Jamoma. 
+ *
+ * @author Timothy Place, Nathan Wolek
+ *
+ * @copyright Copyright © 2015 by Jamoma authors and contributors @n
+ * This code is licensed under the terms of the "BSD 3-Clause License" @n
+ * https://github.com/jamoma/jamoma2/blob/master/LICENSE.md @n
+ */
 
 #pragma once
 
