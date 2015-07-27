fc84c07e5e3ac0faaadc69f352fc1551ae4f2572
nwolek <nw@nathanwolek.com>
2015-07-27 14:47:08 -0400

    simple.cpp doxygen header added

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -1,10 +1,17 @@
-//
-//  main.cpp
-//  JamomaExperiments
-//
-//  Created by Timothy Place on 3/27/15.
-//  Copyright (c) 2015 jamoma. All rights reserved.
-//
+/** @file
+ *
+ * @ingroup jamoma2
+ *
+ * @brief Compiles an example program to test the jamoma2 library.
+ *
+ * @details
+ *
+ * @author Timothy Place, Nathan Wolek
+ *
+ * @copyright Copyright © 2015 by Jamoma authors and contributors @n
+ * This code is licensed under the terms of the "BSD 3-Clause License" @n
+ * https://github.com/jamoma/jamoma2/blob/master/LICENSE.md @n
+ */
 
 #include "Jamoma.h"
 #include "portaudio.h"
