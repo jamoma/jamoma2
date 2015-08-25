810754fef041ea056657b2c4c01e8a43a0378695
Timothy Place <tim@cycling74.com>
2015-08-25 12:26:36 -0500

    unit test integration with cmake. see #39

--- tests/UnitTest.cmake
+++ tests/UnitTest.cmake
@@ -1,6 +1,9 @@
 cmake_minimum_required(VERSION 3.0)
 
 
+enable_testing()
+
+
 include_directories( 
 	"${PROJECT_SOURCE_DIR}/../../includes"
 	"${PROJECT_SOURCE_DIR}/../../includes/core"
@@ -18,4 +21,7 @@ set(CMAKE_C_FLAGS "-fprofile-arcs -ftest-coverage")
 #set(CMAKE_LD_FLAGS "-fprofile-arcs -ftest-coverage")
 SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fprofile-arcs -ftest-coverage")
 
-add_executable(${PROJECT_NAME} ${PROJECT_NAME}.cpp)
+add_executable(${PROJECT_NAME}.test ${PROJECT_NAME}.cpp)
+
+add_test(NAME ${PROJECT_NAME}
+         COMMAND ${PROJECT_NAME}.test)
