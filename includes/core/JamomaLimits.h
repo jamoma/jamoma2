766dbb94336e8c9ad573980467dadfeb019e3291
Timothy Place <tim@cycling74.com>
2015-11-11 21:14:25 -0600

    JamomaLimits.h: changed implementation from templated functions to templated functor classes like we have implemented for interpolation.  This will allow us to specialize Parameter instances with the Limit applied inline and without branching.  Maintained the old templated functions too, though the way I did this may not be ideal since there could be confusion regarding naming.

--- includes/core/JamomaLimits.h
+++ includes/core/JamomaLimits.h
@@ -14,22 +14,22 @@
 
 namespace Jamoma {
 
-
-	template <typename T>
-	T Limit(T input, T low, T high)
+	
+	/**	Determine id a value is a power-of-two. Only works for ints. */
+	template<class T>
+	bool IsPowerOfTwo(T value)
 	{
-		return std::min(std::max(input, low), high);
+		return (value > 0) && ((value & (value-1)) == 0);
 	}
 
 	
 	/**	Limit input to power-of-two values.
-		Non-power-of-two values are increased to the next-highest power-of-two upon return.
-		Only works for ints up to 32-bits.
-		@seealso http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
-	 */
+	 			Non-power-of-two values are increased to the next-highest power-of-two upon return.
+	 			Only works for ints up to 32-bits.
+	 			@seealso http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+	*/
 	template<class T>
-	void TTLimitPowerOfTwo(T value)
-	{
+	void TTLimitPowerOfTwo(T value) {
 		value--;
 		value |= value >> 1;
 		value |= value >> 2;
@@ -41,14 +41,15 @@ namespace Jamoma {
 	}
 	
 	
-	/**	Determine id a value is a power-of-two. Only works for ints. */
-	template<class T>
-	bool IsPowerOfTwo(T value)
+	
+	
+	template <typename T>
+	T Clip(T input, T low, T high)
 	{
-		return (value > 0) && ((value & (value-1)) == 0);
+		return std::min(std::max(input, low), high);
 	}
-
-
+	
+	
 	/** This routine wrapping around the range as much as necessary */
 	template<class T>
 	T Wrap(T input, const T low_bound, const T high_bound)
@@ -60,10 +61,10 @@ namespace Jamoma {
 		else
 			return (-1.0 * fmod(-1.0 * (input - low_bound), fabs((double)low_bound - high_bound)) + high_bound);
 	}
-
-		
+	
+	
 	/** A fast routine for wrapping around the range once.  This is faster than doing an expensive module, where you know the range of the input
-	will not equal or exceed twice the range. */
+	 will not equal or exceed twice the range. */
 	template<class T>
 	T WrapOnce(T input, const T low_bound, const T high_bound)
 	{
@@ -74,8 +75,8 @@ namespace Jamoma {
 		else
 			return ((high_bound + 1) - (low_bound - input));
 	}
-
-
+	
+	
 	/** This routine folds numbers into the data range */
 	template<typename T>
 	T Fold(T input, const T low_bound, const T high_bound)
@@ -89,8 +90,8 @@ namespace Jamoma {
 			return fabs(remainder(input - low_bound, foldRange)) + low_bound;
 		}
 	}
-
-		
+	
+	
 	/** A utility for scaling one range of values onto another range of values. */
 	template<class T>
 	static T Scale(T value, T inlow, T inhigh, T outlow, T outhigh)
@@ -115,6 +116,83 @@ namespace Jamoma {
 		else
 			return((long)(value - 0.5));
 	}
+
+	
+	namespace Limit {
+		
+		template <typename T>
+		class Base {
+		public:
+			Base() = delete;
+		};
+		
+		
+		template <typename T>
+		class None : public Base<T> {
+		public:
+			static T apply(T input, T low, T high) {
+				return input;
+			}
+			
+			T operator()(T input, T low, T high) {
+				return apply(input, low, high);
+			}
+		};
+		
+		
+		template <typename T>
+		class Clip : public Base<T> {
+		public:
+			static T apply(T input, T low, T high) {
+				return Jamoma::Clip<T>(input, low, high);
+			}
+			
+			T operator()(T input, T low, T high) {
+				return apply(input, low, high);
+			}
+		};
+
+		
+		template <typename T>
+		class Wrap : public Base<T> {
+		public:
+			static T apply(T input, T low, T high) {
+				return Jamoma::Wrap(input, low, high);
+			}
+			
+			T operator()(T input, T low, T high) {
+				return apply(input, low, high);
+			}
+		};
+		
+		template <typename T>
+		class WrapOnce : public Base<T> {
+		public:
+			static T apply(T input, T low, T high) {
+				return Jamoma::WrapOnce(input, low, high);
+			}
+			
+			T operator()(T input, T low, T high) {
+				return apply(input, low, high);
+			}
+		};
+		
+		template <typename T>
+		class Fold : public Base<T> {
+		public:
+			static T apply(T input, T low, T high) {
+				return Jamoma::Fold(input, low, high);
+			}
+			
+			T operator()(T input, T low, T high) {
+				return apply(input, low, high);
+			}
+		};
+		
+
+	} // namespace Limit
+	
+	
 
 
 }  // namespace Jamoma
