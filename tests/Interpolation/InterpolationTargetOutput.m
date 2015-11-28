9efe956b8c3dbf6edd17001bf0049043afab7f54
nwolek <nw@nathanwolek.com>
2015-11-27 23:11:02 -0500

    Interpolation: created Octave file to generate target for linear and cubic interpolation. see issue #56

---
+++ tests/Interpolation/InterpolationTargetOutput.m
@@ -0,0 +1,29 @@
+% @file
+% @ingroup 	jamoma2
+% 
+% @brief 		Generates the Expected Target Output for Interpolation using Octave
+%
+% @author		Nathan Wolek
+% @copyright	Copyright (c) 2005-2015 The Jamoma Group, http://jamoma.org.
+% @license		This project is released under the terms of the MIT License.
+
+clear
+
+% starting values
+x0 = -1.0;
+x1 =  2.0;
+x2 =  1.0;
+x3 =  4.0;
+x = [x0,x1,x2,x3];
+
+output_linear = double (1 : 64);
+output_cubic = double (1 : 64);
+
+for i = 1:64
+	current_delta = 1.0 + i / 64;
+	output_linear(i) = interp1(x,current_delta);
+	output_cubic(i) = interp1(x,current_delta,"cubic");
+endfor
+
+save expectedOutput.mat output_linear
+save -append expectedOutput.mat output_cubic
