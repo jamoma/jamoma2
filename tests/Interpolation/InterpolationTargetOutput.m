c62d97f59aa94ec368082e2c958267042527be26
nwolek <nw@nathanwolek.com>
2015-12-02 10:33:01 -0500

    adding Octave code to generate spline ExpectedOutput. issue #56

--- tests/Interpolation/InterpolationTargetOutput.m
+++ tests/Interpolation/InterpolationTargetOutput.m
@@ -18,12 +18,15 @@ x = [x0,x1,x2,x3];
 
 output_linear = double (1 : 64);
 output_hermite = double (1 : 64);
+output_spline = double (1 : 64);
 
 for i = 1:64
 	current_delta = 1.0 + i / 64;
 	output_linear(i) = interp1(x,current_delta);
 	output_hermite(i) = interp1(x,current_delta,"pchip");
+	output_spline(i) = interp1(x,current_delta,"spline");
 endfor
 
 save expectedOutput.mat output_linear
 save -append expectedOutput.mat output_hermite
+save -append expectedOutput.mat output_spline
