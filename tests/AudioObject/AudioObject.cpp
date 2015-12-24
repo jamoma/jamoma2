d451d7fecd61608530540848c91ac275a11d1ed6
nwolek <nw@nathanwolek.com>
2015-12-24 08:33:03 -0500

    SampleRate: four initial tests of range pass. see issue #61.

--- tests/AudioObject/AudioObject.cpp
+++ tests/AudioObject/AudioObject.cpp
@@ -26,7 +26,21 @@ public:
 	
 	void testSampleRateLimits()
 	{
-		// do some testing here
+		
+        // testing the sampleRate limits with a common generator class
+        Jamoma::Sync s;
+        
+        s.sampleRate = 1000;
+        mTest->TEST_ASSERT("setting sampleRate param within range", s.sampleRate == 1000 );
+        
+        s.sampleRate = 500000;
+        mTest->TEST_ASSERT("setting sampleRate param way above range", s.sampleRate == 96000 );
+        
+        s.sampleRate = 96010;
+        mTest->TEST_ASSERT("setting sampleRate param slightly above range", s.sampleRate == 96000 );
+        
+        s.sampleRate = -2000;
+        mTest->TEST_ASSERT("setting sampleRate param below range", s.sampleRate == 1 );
 	}
 	
 };
