b019485beaf9d7ce6cb65254fdb560476ee37688
nwolek <nw@nathanwolek.com>
2015-12-24 08:54:43 -0500

    SampleRate: changing test order and adding final test within range. see issue #61

--- tests/AudioObject/AudioObject.cpp
+++ tests/AudioObject/AudioObject.cpp
@@ -36,12 +36,14 @@ public:
         s.sampleRate = 500000;
         mTest->TEST_ASSERT("setting sampleRate param way above range", s.sampleRate == 96000 );
         
-        s.sampleRate = 96010;
-        mTest->TEST_ASSERT("setting sampleRate param slightly above range", s.sampleRate == 96000 );
-        
         s.sampleRate = -2000;
         mTest->TEST_ASSERT("setting sampleRate param below range", s.sampleRate == 1 );
         
+        s.sampleRate = 96010;
+        mTest->TEST_ASSERT("setting sampleRate param slightly above range", s.sampleRate == 96000 );
+        
+        s.sampleRate = 44100;
+        mTest->TEST_ASSERT("setting sampleRate param within range", s.sampleRate == 44100 );
         
         
         // testing the sampleRate limits again with a common processing class
@@ -53,11 +55,14 @@ public:
         dc.sampleRate = 500000;
         mTest->TEST_ASSERT("setting sampleRate param way above range", dc.sampleRate == 96000 );
         
+        dc.sampleRate = -2000;
+        mTest->TEST_ASSERT("setting sampleRate param below range", dc.sampleRate == 1 );
+        
         dc.sampleRate = 96010;
         mTest->TEST_ASSERT("setting sampleRate param slightly above range", dc.sampleRate == 96000 );
         
-        dc.sampleRate = -2000;
-        mTest->TEST_ASSERT("setting sampleRate param below range", dc.sampleRate == 1 );
+        dc.sampleRate = 44100;
+        mTest->TEST_ASSERT("setting sampleRate param within range", dc.sampleRate == 44100 );
         
 
 	}
