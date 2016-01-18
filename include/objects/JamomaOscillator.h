3f279b2bdc7726c2a5a1859b3d17d7acf7af7b0c
nwolek <nw@nathanwolek.com>
2016-01-18 08:19:08 -0500

    renaming Waveform to Oscillator

---
+++ include/objects/JamomaOscillator.h
@@ -0,0 +1,33 @@
+/** @file
+	
+	@ingroup 	jamoma2
+	
+	@brief 		Generate a basic, periodic waveform using an internal lookup table.
+	
+	@author		Nathan Wolek
+	@copyright	Copyright (c) 2005-2015 The Jamoma Group, http://jamoma.org.
+	@license	This project is released under the terms of the MIT License.
+ */
+
+#pragma once
+
+#include "JamomaAudioObject.h"
+#include "JamomaSync.h"
+
+
+namespace Jamoma {
+    
+    /**	This AudioObject generates a <a href="https://en.wikipedia.org/wiki/Waveform">periodic waveform</a> by filling an internal SampleBundle with the shape to be repeated at a given frequency.
+     This method of sound generation is typically referred to as <a href="https://en.wikipedia.org/wiki/Table-lookup_synthesis">table-lookup</a> or <a href="https://en.wikipedia.org/wiki/Wavetable_synthesis">wavetable</a> synthesis.
+     */
+    class Oscillator : public AudioObject {
+    public:
+        static constexpr Classname classname = { "oscillator" };
+        static constexpr auto tags = { "dspGeneratorLib", "audio", "generator", "oscillator", "wavetable" };
+        
+    private:
+        Jamoma::Sync                mSync;
+        Jamoma::SampleBundle        mLookupTable;
+    };
+
+} // namespace Jamoma
