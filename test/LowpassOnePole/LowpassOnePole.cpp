48b3ceece42193e68b0d715c92cc45b399a66b17
nwolek <nw@nathanwolek.com>
2016-04-19 13:18:59 -0400

    cleaning up after bad copy-pastes in Doxygen comments

--- test/LowpassOnePole/LowpassOnePole.cpp
+++ test/LowpassOnePole/LowpassOnePole.cpp
@@ -1,7 +1,7 @@
 /** @file
 	@ingroup 	jamoma2
  
-	@brief 		Unit test for the Dcblocker class
+	@brief 		Unit test for the LowpassOnePole class
  
 	@author		Timothy Place, Nathan Wolek
 	@copyright	Copyright (c) 2005-2015 The Jamoma Group, http://jamoma.org.
