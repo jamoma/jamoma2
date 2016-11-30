ac213b245a12c94b278cdd851efb2938c52ebecb
Timothy Place <tim@cycling74.com>
2016-11-30 10:53:54 -0600

    Position Dataspace: initially working as demonstrated in the unit test. See #96.

--- include/core/JamomaDataspacePosition.h
+++ include/core/JamomaDataspacePosition.h
@@ -18,8 +18,8 @@ namespace Jamoma {
 	namespace Dataspace {
 			
 	
-		/** Names of all units for the temperature dataspace. 
-		 */
+		/// Names of all units for the position dataspace.
+
 		enum class PositionUnit : uint32_t {
 			cart3D			= Hash("cart3D"),		///< Cartesian 3Db(xyz), using SpatDIF coordinate conventions
 			xyz				= Hash("xyz"),			///< Cartesian 3Db(xyz), using SpatDIF coordinate conventions
@@ -36,21 +36,22 @@ namespace Jamoma {
 			daz				= Hash("daz")			///< 3D cylindrical coordinates, expressed as polar coordinates and height (horizontal distance, azimuth, height(z))
 			*/
 		};
-		
-		
-		
-		
-		// Cartesian 3D is the neutral unit, so it is a pass-through
+
+
+		/// A triplet of values of type T
+		template<typename T>
+		using PositionValue = std::array<T,3>;
+
+
+		/// Cartesian 3D is the neutral unit, so it is a pass-through
 		template <class T>
-		class Cartesian3DUnit : public UnitBase std::array<T,3> {
+		class Cartesian3DUnit : public UnitBase<PositionValue<T>> {
 		public:
-			T toNeutral(const T& input) const
-			{
+			PositionValue<T> toNeutral(const PositionValue<T>& input) const override {
 				return input;
 			}
 			
-			T fromNeutral(const T& input) const
-			{
+			PositionValue<T> fromNeutral(const PositionValue<T>& input) const override {
 				return input;
 			}
 		};
@@ -60,30 +61,30 @@ namespace Jamoma {
 		// This implies that
 		//		toNeutral() => receives 2 values, returns 3
 		//		fromNeutral	=> receives 3 values, returns 2
+		// We implement this by padding the PositionValue with zeroes.
+
 		template <class T>
-		class Cartesian2DUnit : public UnitBase std::array<T,3> {
+		class Cartesian2DUnit : public UnitBase<PositionValue<T>> {
 		public:
-			T toNeutral(const T& input) const
-			{
-				return {{ input[0], input[1], 0. }};
+			PositionValue<T> toNeutral(const PositionValue<T>& input) const override {
+				return {{ input[0], input[1], 0.0 }};
 			}
 			
-			T fromNeutral(const T& input) const
-			{
-				return {{ input[0], input[1] }};
+			PositionValue<T> fromNeutral(const PositionValue<T>& input) const override {
+				return {{ input[0], input[1], 0.0 }};
 			}
 		};
 		
 		
-		/**	The Position Dataspace.
-		 */
+		///	The Position Dataspace.
+
 		template <class T,PositionUnit U>
 		class Position /*: public Dataspace*/ {
 			
 			/**	Mapping from unit names to actual unit converter objects.
 				TODO: make this a static so that we don't have to spend resources on it for all instances
 			 */
-			std::unordered_map<PositionUnit, UnitBase<T>*>	sUnits = {
+			std::unordered_map<PositionUnit, UnitBase<PositionValue<T>>*>	sUnits = {
 				{PositionUnit::cart3D, 		new Cartesian3DUnit<T>()},
 				{PositionUnit::xyz, 		new Cartesian3DUnit<T>()},
 				{PositionUnit::cart2D, 		new Cartesian2DUnit<T>()},
@@ -92,24 +93,24 @@ namespace Jamoma {
 
 			
 			/**	The native unit to/from which we perform conversions.	*/
-			const UnitBase<T>* mUnit = sUnits[U];
+			const UnitBase<PositionValue<T>>* mUnit = sUnits[U];
 
 			
 		public:
 			
 			/**	Conversion function where the unit is passed as enum selection.	*/
-			T operator()(const T& x, const PositionUnit& unit = U)
+			PositionValue<T> operator()(const PositionValue<T>& x, const PositionUnit& unit = U)
 			{
 				return mUnit->fromNeutral( sUnits[unit]->toNeutral(x) );
 			}
 
-			T operator()(const T& x, uint32_t unit)
+			PositionValue<T> operator()(const PositionValue<T>& x, uint32_t unit)
 			{
 				return (*this)(x, (PositionUnit)unit);
 			}
 		
 			/**	Conversion function where the unit is passed as a string.	*/
-			T operator()(const T x, const char* str)
+			PositionValue<T> operator()(const PositionValue<T> x, const char* str)
 			{
 				return (*this)( x, (PositionUnit)Hash(str) );
 			}
